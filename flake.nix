ddc4eb410d003d69ba13421539c525d315886eee
ClausNMadsen <claus.normann@hotmail.com>
2024-04-28 17:17:06 +0200

    First commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+    inputs = {
+        utils.url = "github:numtide/flake-utils";
+        fenix = {
+            url = "github:nix-community/fenix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+    };
+
+    outputs = { self, fenix, nixpkgs, utils }:
+        utils.lib.eachDefaultSystem (system:
+                let
+                pkgs = import nixpkgs { inherit system; };
+                in
+                {
+                devShells.default = pkgs.mkShell {
+                    nativeBuildInputs =
+                        [
+                            fenix.packages.${system}.complete.toolchain
+                        ];
+                };
+        });
+}
