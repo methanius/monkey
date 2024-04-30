1298118890e2329657f29d82ec0d8ac6dc7591ee
ClausNMadsen <claus.normann@hotmail.com>
2024-04-30 17:35:22 +0200

    Add pre-commit to flake

--- flake.nix
+++ flake.nix
@@ -6,19 +6,34 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
         nixpkgs.url = "nixpkgs/nixos-unstable";
+        pre-commit-hooks = {
+            url = "github:cachix/pre-commit-hooks.nix";
+            # inputs.nixpkgs.folows = "nixpkgs";
+            # inputs.flake-utils.folows = "utils";
+        };
     };
 
-    outputs = { self, fenix, nixpkgs, utils }:
+    outputs = { self, fenix, nixpkgs, utils, pre-commit-hooks }:
         utils.lib.eachDefaultSystem (system:
-                let
+            let
                 pkgs = import nixpkgs { inherit system; };
-                in
-                {
-                devShells.default = pkgs.mkShell {
-                    nativeBuildInputs =
-                        [
-                            fenix.packages.${system}.complete.toolchain
-                        ];
+            in
+            {
+                checks = {
+                    pre-commit-check = pre-commit-hooks.lib.${system}.run {
+                        src = ./.;
+                        hooks = {
+                            rustfmt.enable = true;
+                            clippy.enable = true;
+                        };
+                    };
                 };
+            devShells.default = pkgs.mkShell {
+                inherit (self.checks.${system}.pre-commit-check) shellHook;
+                nativeBuildInputs =
+                    [
+                        fenix.packages.${system}.complete.toolchain
+                    ];
+            };
         });
 }
